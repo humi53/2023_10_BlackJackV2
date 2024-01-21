b0e44590a483707cb2a08c854437081ee9f1211f
humi53 <j.k.humi9@gmail.com>
2024-01-21 18:11:47 +0900

    dealer 카드합산메소드(player와 동일) & 첫장 ace, 10 확인 메소드

--- Aenean/src/com/yopheu/aenean/models/DealerDataDto.java
+++ Aenean/src/com/yopheu/aenean/models/DealerDataDto.java
@@ -28,6 +28,76 @@ public class DealerDataDto {
 		return isOpen;
 	}
 	
+	// 첫장 Ace 확인
+	public boolean isAce() {
+		boolean result = false;
+		if(!arrCard.isEmpty() 
+				&& arrCard.get(0).getDenomination() == Denomination.NA) {
+			 result = true;
+		}
+		return result;
+	}
+	
+	// 첫장 10 확인
+	public boolean is10() {
+		boolean result = false;
+		if(!arrCard.isEmpty() 
+				&& arrCard.get(0).getDenomination().getValue() == 10) {
+			 result = true;
+		}
+		return result;
+	}
+	
+	// 블랙잭 확인.
+	public boolean isBlackJack() {
+		boolean result = false;
+		// 2장이면서, 21일때
+		if(arrCard.size() == 2 
+				&& calcCardSum(arrCard) == 21) {
+			result = true;
+		}else {
+			result = false;
+		}
+		return result;
+	}
+		
+	// 일반카드 합산
+	public int getCardSum() {
+		return calcCardSum(arrCard);
+	}
+	
+	private int calcCardSum(ArrayList<Card> cards) {
+		int result = 0;
+		boolean ace = false;
+		// A 카드가 1장 이상 있을때 일반적인 처리
+		// 첫A는 11 다음장 A는 1로 처리.
+		for(Card card : cards) {
+			Denomination card_denomi = card.getDenomination();			
+			if(ace && card_denomi == Denomination.NA) {
+				result += 1;
+			}else {
+				if(card_denomi == Denomination.NA) {
+					ace = true;
+				}
+				result += card.getDenomination().getValue();				
+			}
+		}
+		// A 카드가 1장이상 있으며 21을 넘어갈때 처리
+		// 모든 A 카드를 1로 처리.
+		if(ace && result > 21) {
+			result = 0;
+			for(Card card : cards) {
+				Denomination card_denomi = card.getDenomination();
+				if(card_denomi == Denomination.NA) {
+					result += 1;
+				}else {
+					result += card.getDenomination().getValue();
+				}
+			}
+		}
+		return result;
+	}
+	
 	// 카드 추가
 	public void addCard(Card card) {
 		this.arrCard.add(card);
