b0a2e838283fe1a352e1f0366659191fa2c9e8be
humi53 <j.k.humi9@gmail.com>
2024-04-16 12:17:18 +0900

    ANSI 색상 종류 추가

--- Aenean/src/com/yopheu/games/aenean/config/ANSIColor.java
+++ Aenean/src/com/yopheu/games/aenean/config/ANSIColor.java
@@ -2,12 +2,13 @@ package com.yopheu.games.aenean.config;
 
 public enum ANSIColor {
 	BLACK("\u001B[30m"),
+	RED("\u001B[31m"),
+	GREEN("\u001B[32m"),
+	YELLOW("\u001B[33m"),
+	BLUE("\u001B[34m"),
+	PURPLE("\u001B[35m"),
+	CYAN("\u001B[36m"),
     WHITE("\u001B[37m"),
-    YELLOW("\u001B[33m"),
-    GREEN("\u001B[32m"),
-    RED("\u001B[31m"),
-    PURPLE("\u001B[35m"),
-    CYAN("\u001B[36m"),
     END("\u001B[0m");
 	private final String ANSI;
 
